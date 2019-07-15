4b6d6ff77615cd9bee0aa348ce97e549d2d6fbe0
Phan Huu Tuan <phantuan.ktd@gmail.com>
2019-07-15 08:42:17 +0700

    components

---
+++ template-parts/components/style-grid-2-4xn-first.php
@@ -0,0 +1,22 @@
+<div class="col-sm-6 pi-style-box">
+	<div class="pi-news-item">
+		<div class="pi-news-thumb">
+			<div class="pi-news-img">
+				<a href="<?php the_permalink() ?>">
+					<img src="http://localhost:8888/wp-content/uploads/2019/07/smart-industry.jpg"/>
+				</a>
+			</div>
+		</div>
+		<div class="pi-news-article">
+			<div class="pi-news-pubDate">
+				<span class="pi-news-sm-tag pull-left">
+					<?php pi_get_the_category(); ?>
+				</span>
+				<span class="pubDate pull-right"><i class="fa fa-clock-o" aria-hidden="true"></i> <?php echo get_the_date('n-j-Y'); ?></span>
+			</div>
+			<div class="pi-news-name">
+				<a href="<?php the_permalink() ?>"><?php echo get_the_title() ?></a>
+			</div>
+		</div>
+	</div>
+</div>
